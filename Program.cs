020a8c1c37b2a331c85066c76f5fb00128e5524d
Andrii Kurdiumov <kant2002@gmail.com>
2020-03-02 13:24:39 +0600

    Add example for Threading in CoreRT

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace threading_corert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(WriteInLoop);
+            Thread thread2 = new Thread(WriteInLoop);
+            Console.WriteLine("Hello World!");
+            thread1.Start(1);
+            Thread.Sleep(500);
+            thread2.Start(2);
+        }
+
+        public static void WriteInLoop(object state)
+        {
+            int thread_id = (int)state;
+            Console.WriteLine($"From thread {thread_id}");
+        }
+    }
+}
